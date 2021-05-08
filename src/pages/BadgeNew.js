e8a74af774de7f9cbcc66f94498339bb4304c5e8
Royvargas55 <royvargas55@gmail.com>
2021-05-08 15:45:22 -0600

    Adding styles badge new page

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import Navbar from '../components/Navbar'
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
     render() {
         return(
             <div>
                 <Navbar />
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="logo"/>
+                </div>
             </div>
         )
     }
